02ea4966858ca23656a25dc2d625223fac7bc0b6
Riki Melonk <rikymelonk00@gmail.com>
2024-09-30 19:06:08 +0700

    Tutorial MERN #21: Instalasi dan Setup ReactJs + DaysiUI

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button class="btn btn-primary">Primary</button>
+    </>
+  )
+}
+
+export default App
