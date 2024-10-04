7b1722759ee0a688a1eabde17b7f5b9d18d64138
Riki Melonk <rikymelonk00@gmail.com>
2024-10-04 16:17:08 +0700

    Buat Koneksi Antar BACKEND dan FRONTEND dengan VITE

--- src/pages/HomeView.jsx
+++ src/pages/HomeView.jsx
@@ -1,11 +1,12 @@
-// import axios from 'axios'
+import axios from 'axios'
 
-// // try {
-// //   const data = await axios.get('/api/v1/product')
-// //   console.log(data)
-// // } catch (error) {
-// //   console.log(error)
-// // }
+try {
+  const data = await axios.get('/api/v1/product')
+  console.log(data)
+  console.log('Data yang diterima:', data);
+} catch (error) {
+  console.log(error)
+}
 
 const HomeView = () => {
   return <div>HomeView</div>
