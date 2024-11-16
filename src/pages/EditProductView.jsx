4b9e0416c5a93499f5679b8317900375928204df
Riki Melonk <rikymelonk00@gmail.com>
2024-11-17 02:26:14 +0700

    CRUD edit PRODUCT berdasarkan ID

--- src/pages/EditProductView.jsx
+++ src/pages/EditProductView.jsx
@@ -1,5 +1,99 @@
+import { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import customAPI from '../api'
+import Loading from '../components/Loading'
+import FormInput from '../components/form/FormInput'
+import FormTextArea from '../components/form/FormTextArea'
+import FormSelect from '../components/form/FormSelect'
+import { toast } from 'react-toastify'
+
 const EditProductView = () => {
-  return <div>EditProductView</div>
+  const categories = ['Bunga Meja', 'Hand Bouquet', 'Bunga Papan']
+  const [product, setProduct] = useState(null)
+  const { id } = useParams()
+  const navigate = useNavigate()
+
+  const getProductId = async () => {
+    const { data } = await customAPI.get(`/product/${id}`)
+    setProduct(data.data)
+    // console.log(data.data)
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    event.preventDefault()
+    const dataForm = new FormData(event.target)
+
+    const data = Object.fromEntries(dataForm)
+    // console.log(data)
+    try {
+      // create product
+      await customAPI.put(`/product/${id}`, {
+        // image: responseFileUpload.data.url,
+        // ...data,
+        name: data.name,
+        price: data.price,
+        stock: data.stock,
+        description: data.description,
+        category: data.category,
+      })
+      toast.info('Product Berhasil Diubah')
+      navigate('/products')
+    } catch (error) {
+      const errorMessage = error.response.data.message
+      toast.error(errorMessage)
+    }
+  }
+
+  useEffect(() => {
+    getProductId()
+  }, [])
+
+  return (
+    <>
+      {product ? (
+        <form onSubmit={handleSubmit} encType="multipart/form-data">
+          <FormSelect
+            label="Pilih Category"
+            name="category"
+            list={categories}
+            defaultValue={product.category}
+          />
+          <FormInput
+            label="Nama Product"
+            name="name"
+            type="text"
+            defaultValue={product.name}
+          />
+          <FormInput
+            label="Harga Product"
+            name="price"
+            type="number"
+            defaultValue={product.price}
+          />
+          <FormInput
+            label="Stock Product"
+            name="stock"
+            type="number"
+            defaultValue={product.stock}
+          />
+          <FormTextArea
+            label="Description Product"
+            name="description"
+            defaultValue={product.description}
+          />
+          <input
+            type="submit"
+            value="Edit"
+            className="btn btn-primary btn-block mt-5 btn-md"
+          />
+        </form>
+      ) : (
+        <Loading />
+      )}
+    </>
+  )
 }
 
 export default EditProductView
